d30557e515116237041b45628383c32eadd5688f
Debora <deboraa.bras@gmail.com>
2023-06-29 16:59:42 -0300

    Aula 4 - objetos e propriedades

---
+++ aula4/objetos.js
@@ -0,0 +1,13 @@
+// objetos (tipos de referencias) tem finalidade de juntar informações em um unico lugar podendo acessar pela propriedade dele
+//ex:
+
+let nome = 'Debora Cunha && 01'; 
+let idade = 30; 
+let estaAprovado = true; 
+
+let pessoa = {  //objeto 
+    nome: 'Debora Cunha',//propriedade com seu valor (chave e valor)
+    idade: 30,
+    estaAprovado: true
+};
+console.log (pessoa)
