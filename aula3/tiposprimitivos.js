9e9e566296a802696043a71e4e9bb86a46b6d325
Debora <deboraa.bras@gmail.com>
2023-06-29 15:56:53 -0300

    Aula de tipos primitivos

---
+++ aula3/tiposprimitivos.js
@@ -0,0 +1,5 @@
+let nome = 'Debora Cunha && 01'; // string literal (pode ter numero, carcter e letras junto)
+let idade = 30; // number literal 
+let estaAprovado = true; //boolean
+let sobrenome = undefined ; // undefined (sem definição) - pouco usado
+let corSelecionado = null; // redefinir o valor de cor
