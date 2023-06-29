b0dfca8a40551610c0ae2a853cfb40753656816d
Debora <deboraa.bras@gmail.com>
2023-06-29 12:01:58 -0300

    Aula 1 Variaveis no Console

--- aula1/variaveis.js
+++ aula1/variaveis.js
@@ -1,1 +1,15 @@
-var nome, idade, email;
+console.log ('bem vindo ao curso')
+console.log ('testando o console')
+
+// maneira mais recomendada de usar variavel é a camelCase - 1ª palavra minuscula e as seguintes com a inicial maiuscula
+//ex:
+let nomeCompleto = 'Debora Cunha';
+console.log (nomeCompleto)
+
+let idade = 5;
+console.log(idade);
+
+let altura = 154;
+console.log(altura);
+
+
