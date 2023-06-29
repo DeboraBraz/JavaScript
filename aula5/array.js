b593bb0e22d2e6d23513abb73a6c61bf6bc05df3
Debora <deboraa.bras@gmail.com>
2023-06-29 17:13:28 -0300

    Aula 5 - Array

---
+++ aula5/array.js
@@ -0,0 +1,9 @@
+// array é um conjunto de dados que pode ser acessado por um indice
+//ex:
+let meusDados = ['Debora',30,154,true];
+
+// caso eu queira acessar todo o array no console:
+
+
+// caso eu queira acessar somente um valor do array preciso usar a posição indice que começa sempre com 0:
+console.log (meusDados[3]);
