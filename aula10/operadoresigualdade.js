f85bbc42422483a603eb2d9e4d682e168659a1f2
Debora <deboraa.bras@gmail.com>
2023-07-10 17:50:27 -0300

    operadores de igualdade

---
+++ aula10/operadoresigualdade.js
@@ -0,0 +1,16 @@
+//operadores de igualdade
+
+//igualdade estrita  (MAIS RECOMENDADA)
+//comparando dois numerais 1 | compara tipo e valores (usa 3 '=') - valores e tipos iguais
+//true: 
+console.log(1 === 1);
+//comparando um numeral 1 e um string 1 - tipos distintos
+//false
+console.log('1' === 1);
+
+
+//igualdade solta | compara apenas os valores e ignora o tipo (usa 2 '=')
+//true
+console.log(1 == 1);
+//true | pois não compara tipos apenas valores 
+console.log('1' == 1);
