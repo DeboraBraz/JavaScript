8dddb2d4eb6cf23225768b0a6211fde7ddc13220
Debora <deboraa.bras@gmail.com>
2023-07-10 17:25:20 -0300

    operadores aritimeticos

---
+++ aula8/operadoresaritimeticos.js
@@ -0,0 +1,20 @@
+//operadores aritimeticos 
+//operadores de atribuição 
+//operadores de comparação 
+//operadores logicos 
+//operadores bitwise
+
+//operadores aritimeticos (+,-,*,/, **(numero vezes o mesmo numero))
+let salario = 100;
+//ex 
+console.log (salario + salario);
+console.log (salario - salario);
+console.log (salario * salario);
+console.log (salario / salario);
+console.log (5 ** 5);
+
+
+//++ --
+let idade=18;
+//console.log (++idade);
+console.log (--idade);
