361b53cd41d76e6e3a1ca1f79ebe668e0066aaba
Debora <deboraa.bras@gmail.com>
2023-07-10 17:37:19 -0300

    operadores de atribuiçao

---
+++ aula9/operadoresatribuicao.js
@@ -0,0 +1,11 @@
+// o operador = atribui para a variavel o valor passado
+let valorCurso=100;
+console.log(valorCurso);
+
+//variações - valorCurso = valorCurso + valorCurso. É o mesmo que:
+valorCurso += valorCurso
+console.log(valorCurso);
+
+//assim como:
+valorCurso -= valorCurso
+console.log(valorCurso);
